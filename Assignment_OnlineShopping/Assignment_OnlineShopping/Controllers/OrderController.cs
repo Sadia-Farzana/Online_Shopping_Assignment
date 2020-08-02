74708ed46ac92256d8e6fcdbd41491b203ccbd0e
17-33890-1 <17-33890-1@student.aiub.edu>
2020-08-02 16:14:37 +0600

    OrderPage

--- Assignment_OnlineShopping/Assignment_OnlineShopping/Controllers/OrderController.cs
+++ Assignment_OnlineShopping/Assignment_OnlineShopping/Controllers/OrderController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Assignment_OnlineShopping.Models;
+using Assignment_OnlineShopping.Models.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +12,29 @@ namespace Assignment_OnlineShopping.Controllers
 {
     public class OrderController : Controller
     {
+        HalloweenContext ord = new HalloweenContext();
+
         // GET: Order
-        public ActionResult Index()
+        public ActionResult Order()
         {
+            return View(ord.Products.ToList());
+        }
+      
+        /*public ActionResult Order()
+        {
+         
             return View();
         }
+
+        [HttpPost]
+
+        public ActionResult Create(LineItem p)
+        {
+
+            ord.LineItems.Add(p);
+            ord.SaveChanges();
+            return RedirectToAction("Order");
+
+        }*/
     }
 }
